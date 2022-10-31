69552418b4f38ef21b075117281cfe654519533e
guedesclaudio <cgsarmentosilva@gmail.com>
2022-10-31 15:00:32 -0300

    feat : created project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "../styles/global.style"
+import SignIn from "../pages/SignIn"
+
+export default function App() {
+
+    return (
+        <>
+            <GlobalStyle/>
+            <SignIn/>
+        </>
+        
+    )
+}
